48ad43bd3d7ae655dffd6286b6f621f1897e1d40
Kristinn Ingi Reynisson <kristinnr17@ru.is>
2018-09-04 18:08:35 +0000

    virkar hjá mér en ekki í mimir

---
+++ sequence.py
@@ -0,0 +1,17 @@
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+
+a = 1
+b = 2
+c = 3
+
+for i in range(1, n+1):
+    if i == 1:
+        print(a)
+        print(b)
+        print(c)
+    else:
+        d = a + b + c
+        a = b
+        b = c
+        c = d
+        print(d)
