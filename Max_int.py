e422fb4ab5183aa21b1402b9e14abf6ceaf1968f
Kristinn Ingi Reynisson <kristinnr17@ru.is>
2018-09-04 14:41:12 +0000

    Changed num_int to 0 and  while >= to 0

--- Max_int.py
+++ Max_int.py
@@ -1,6 +1,6 @@
-num_int = 1
+num_int = 0
 max_int = 0
-while num_int > 0:
+while num_int >= 0:
     num_int = int(input("Input a number: "))    # Do not change this line
     if num_int > max_int:
         max_int = num_int
